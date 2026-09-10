d59d0afd51846ba355ba1e1a39b8a6a9fbbcf7ed
binfu-seek <binfu.seek@gmail.com>
2026-09-10 13:25:12 +1000

    Add data model - TodoItemModel

---
+++ TodoList.Api/Models/TodoItemModel.cs
@@ -0,0 +1,20 @@
+﻿namespace TodoList.Api.Models
+{
+    public enum TodoItemStatus
+    {
+        New,
+        InProgress,
+        Done,
+        Pending,
+        Cancelled
+    }
+    public class TodoItemModel
+    {
+        public Guid Guid { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public TodoItemStatus Status { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Notes { get; set; }
+    }
+}
