a033695c1c6d50b6272b367935e1ff15540dd74e
binfu-seek <binfu.seek@gmail.com>
2026-09-10 13:44:19 +1000

    Add IRepository interface

---
+++ TodoList.Api/Repositories/IRepository.cs
@@ -0,0 +1,11 @@
+﻿using TodoList.Api.Models;
+
+namespace TodoList.Api.Repositories
+{
+    public interface IRepository
+    {
+        Task<IEnumerable<TodoItemModel>> GetTodos();
+        Task<TodoItemModel> GetTodoById(Guid guid);
+        Task<TodoItemModel> DeleteTodo(Guid guid);
+    }
+}
