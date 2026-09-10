8d9ceb296e9fa9a6e9aa72a28764d8288e066464
binfu-seek <binfu.seek@gmail.com>
2026-09-10 14:31:53 +1000

    Add Repository and TodoService classes without implementation

---
+++ TodoList.Api/Repositories/Repository.cs
@@ -0,0 +1,29 @@
+﻿using TodoList.Api.Models;
+
+namespace TodoList.Api.Repositories
+{
+    public class Repository : IRepository
+    {
+        public Repository() { }
+
+        public Task<TodoItemModel> CreateTodoAsync(TodoItemModel newTodo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoItemModel> DeleteTodoAsync(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoItemModel> GetTodoByIdAsync(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TodoItemModel>> GetTodosAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ TodoList.Api/Services/TodoService.cs
@@ -0,0 +1,34 @@
+﻿using TodoList.Api.DTOs;
+using TodoList.Api.Repositories;
+
+namespace TodoList.Api.Services
+{
+    public class TodoService : ITodoService
+    {
+        private readonly IRepository _repository;
+        public TodoService(IRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public Task<TodoResponseDto> CreateTodoAsync(TodoRequestDto newTodo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoResponseDto> DeleteTodoAsync(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TodoResponseDto> GetTodoByIdAsync(Guid guid)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TodoResponseDto>> GetTodosAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
