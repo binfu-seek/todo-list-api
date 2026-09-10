5b72ebd5af16e47628a8c8b19a640a77ac421990
binfu-seek <binfu.seek@gmail.com>
2026-09-10 14:39:47 +1000

    Registered dependency injection for both repository and TodoService

--- TodoList.Api/Program.cs
+++ TodoList.Api/Program.cs
@@ -1,8 +1,13 @@
+using TodoList.Api.Repositories;
+using TodoList.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddSingleton<IRepository, Repository>();
+builder.Services.AddScoped<ITodoService, TodoService>();
 
 var app = builder.Build();
 
