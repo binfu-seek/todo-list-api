b37e46a8e06f2247cd60938298118ea63688b750
binfu-seek <binfu.seek@gmail.com>
2026-09-11 00:32:25 +1000

    Adjust TodoRequestDto fields

--- TodoList.Api/DTOs/TodoRequestDto.cs
+++ TodoList.Api/DTOs/TodoRequestDto.cs
@@ -6,7 +6,6 @@ namespace TodoList.Api.DTOs
     {
         public string Title { get; set; }
         public string Description { get; set; }
-        public TodoItemStatus Status { get; set; }
         public string Notes { get; set; }
     }
 }
