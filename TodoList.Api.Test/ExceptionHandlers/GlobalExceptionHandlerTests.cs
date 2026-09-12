fb5b2df8eb19be8342f9b44d547acb37ec309cd4
binfu-seek <binfu.seek@gmail.com>
2026-09-12 17:16:13 +1000

    Add empty test files for Repository and GlobalExceptionHandler

---
+++ TodoList.Api.Test/ExceptionHandlers/GlobalExceptionHandlerTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList.Api.Test.ExceptionHandlers
+{
+    public class GlobalExceptionHandlerTests
+    {
+        // TODO: Implement unit tests for the GlobalExceptionHandler class.
+    }
+}
---
+++ TodoList.Api.Test/Repositories/RepositoryTests.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TodoList.Api.Test.Repositories
+{
+    public class RepositoryTests
+    {
+        // TODO: Implement unit tests for the Repository class.
+    }
+}
