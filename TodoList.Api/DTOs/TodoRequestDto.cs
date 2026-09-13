315f4f7cce7334e3b4c1553f6bcb4a38e5dfd7ac
binfu-seek <binfu.seek@gmail.com>
2026-09-13 22:33:30 +1000

    Update max length of title field to 100

--- TodoList.Api/DTOs/TodoRequestDto.cs
+++ TodoList.Api/DTOs/TodoRequestDto.cs
@@ -6,7 +6,7 @@ namespace TodoList.Api.DTOs
     public class TodoRequestDto
     {
         [Required(ErrorMessage = "The todo title cannot be empty.")]
-        [StringLength(10, MinimumLength = 3, 
+        [StringLength(100, MinimumLength = 3, 
             ErrorMessage = "The title must be between 3 and 100 characters long.")]
         public string Title { get; set; }
         public string Description { get; set; }
